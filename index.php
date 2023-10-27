ad42aab42a6eabfcea3dcab2c3a14638f2e27fc8
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-10-27 18:25:10 +0800

    Add send qr data

--- index.php
+++ index.php
@@ -13,22 +13,18 @@
 
 <body>
 
-  <main>
-
-    <div class="container-lg">
-      <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-          <!-- qr scanner -->
-          <div id="reader"></div>
-        </div>
-        <div class="col-md-3"></div>
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <!-- qr scanner -->
+        <div id="reader"></div>
       </div>
+      <div class="col-md-3"></div>
     </div>
+  </div>
 
-    <div id="result"></div>
-
-  </main>
+  <div id="result" class="text-center"></div>
 
   <script>
     const scanner = new Html5QrcodeScanner('reader', {
@@ -39,20 +35,61 @@
       fps: 20,
     });
 
+    // Flag to track whether a QR code has been successfully captured
+    let qrCodeCaptured = false;
+
     scanner.render(success, error);
 
     function success(result) {
-      document.getElementById('result').innerHTML = `
-        <h2>SUCCESS</h2>
-        <p><a>${result}</a></p>
-        `;
+      if (!qrCodeCaptured) {
+        qrCodeCaptured = true;
 
-      scanner.clear();
-      document.getElementById('reader').remove();
+        // Send the captured data to a PHP script
+        sendDataToPHP(result);
+
+        // Display success message
+        document.getElementById('result').innerHTML = `
+        <h1>${result}</h1>
+      `;
+        // Reset the scanner and allow capturing another QR code
+        scanner.reset();
+        qrCodeCaptured = false;
+
+        // Render the scanner again after a short delay
+        setTimeout(function () {
+          scanner.render(success, error);
+        }, 1000); // Adjust the delay as needed
+      }
+
+      // scanner.clear();
+      // document.getElementById('reader').remove();
     }
 
     function error(err) {
       console.error(err);
+    }
+
+    function sendDataToPHP(data) {
+      // Create a FormData object to send data to PHP
+      const formData = new FormData();
+      formData.append('qrcode_data', data);
+
+      // Make an HTTP request to the PHP script
+      fetch('sendQrData.php', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(response => response.text())
+        .then(responseData => {
+          // Handle the response from the PHP script if needed
+          console.log(responseData);
+
+          // Allow capturing another QR code
+          qrCodeCaptured = false;
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   </script>
 </body>
