b9422ea0e4a71d5f0f9c8f02bfb9833e3248c91b
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-11-07 01:20:40 +0800

    Edit title

--- admin/include/header.php
+++ admin/include/header.php
@@ -6,5 +6,5 @@
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <script src="../bootstrap/js/bootstrap.bundle.js"></script>
     <link rel="stylesheet" href="../node_modules/boxicons/css/boxicons.css"></link>
-    <title>Document</title>
+    <title>QR Code System</title>
 </head>
