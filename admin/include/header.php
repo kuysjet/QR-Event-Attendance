7ca4892ce964221979ccbc196f2c27c653ba85b9
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-11-07 01:02:11 +0800

    Add link boxicons css

--- admin/include/header.php
+++ admin/include/header.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <script src="../bootstrap/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="../node_modules/boxicons/css/boxicons.css"></link>
     <title>Document</title>
 </head>
