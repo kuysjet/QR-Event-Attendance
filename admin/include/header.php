9c6c4bef0bab9a4a97d245ccf000d27e7b1b45e2
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-11-07 00:37:57 +0800

    Link bootstrap js bundle

--- admin/include/header.php
+++ admin/include/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script src="../bootstrap/js/bootstrap.bundle.js"></script>
     <title>Document</title>
 </head>
