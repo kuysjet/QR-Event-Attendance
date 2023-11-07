72197bb847bfd26d07df7258098db043df522257
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-11-08 01:16:34 +0800

    Place jquery and datatables link

--- admin/include/header.php
+++ admin/include/header.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <script src="../bootstrap/js/bootstrap.bundle.js"></script>
+    <script type="text/javascript" src="../bootstrap/js/bootstrap.bundle.js"></script>
     <link rel="stylesheet" href="../node_modules/boxicons/css/boxicons.css"></link>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="../node_modules/datatables.net-dt/css/jquery.dataTables.css" />
+    <script type="text/javascript" src="../node_modules/datatables.net/js/jquery.dataTables.js"></script>
+    
     <title>QR Code System</title>
 </head>
